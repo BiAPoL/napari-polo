20e739a69525b4d62755aaca60abdc6896e8cc55
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-05-04 14:55:22 +0200

    Add a regression test for generate_clean_orientation_image function

--- napari_polo/_tests/test_function.py
+++ napari_polo/_tests/test_function.py
@@ -1,3 +1,10 @@
-# from napari_polo import threshold, image_arithmetic
+import numpy as np
+from skimage.io import imread
 
-# add your tests here...
+
+def test_generate_clean_orientation_image():
+    image = imread("liver_example.tif")
+    from napari_polo import generate_clean_orientation_image
+    result = generate_clean_orientation_image(image)
+    expected = imread("liver_example_cleaned.tif")
+    assert np.allclose(result, expected, atol=0.001)
