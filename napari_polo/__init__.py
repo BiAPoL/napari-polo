df7de6e726a0dc7d4e07a19d320e14b800ef2540
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-05-04 13:24:34 +0200

    Reorder importing of functions in init

--- napari_polo/__init__.py
+++ napari_polo/__init__.py
@@ -1,8 +1,3 @@
 __version__ = "0.1.0"
 
-from ._function import generate_clean_orientation_image
-
-
-
-
-from ._function import napari_experimental_provide_function
+from ._function import generate_clean_orientation_image, napari_experimental_provide_function
