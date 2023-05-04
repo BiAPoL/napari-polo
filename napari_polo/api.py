8973f1dfbdd7e71e388db599c6eca6f3eca05e96
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-05-04 14:13:50 +0200

    Change output of generate_clean_orientation_image to the float_image

--- napari_polo/api.py
+++ napari_polo/api.py
@@ -1,3 +1,4 @@
+import napari.types
 from napari_plugin_engine import napari_hook_implementation
 from napari_time_slicer import time_slicer
 from napari_tools_menu import register_function
@@ -6,9 +7,9 @@ from napari_tools_menu import register_function
 @register_function(menu="Segmentation / labeling > Generate clean labeled orientation image")
 @time_slicer
 def generate_clean_orientation_image(raw_orientation_image: 'napari.types.ImageData', step_size: int = 10,
-                                     erosion_footprint_width: int = 3) -> 'napari.types.LabelsData':
+                                     erosion_footprint_width: int = 3) -> 'napari.types.ImageData':
     from ._utilities import prepro_orientation
-    from ._core_functions import percentile_pairs, filter_arr_vals, label_expansion
+    from ._core_functions import percentile_pairs, filter_arr_vals, label_expansion, mask_adj_obj
 
     normalized_image = prepro_orientation([raw_orientation_image])[0]
 
@@ -18,7 +19,9 @@ def generate_clean_orientation_image(raw_orientation_image: 'napari.types.ImageD
 
     labeled_images, counts = label_expansion(filtered_images, noise_se_size=erosion_footprint_width)
 
-    return labeled_images.astype(int)
+    binary_image, float_image, label_image, count_objects = mask_adj_obj(normalized_image, labeled_images)
+
+    return float_image
 
 
 @napari_hook_implementation
